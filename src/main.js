5a029bdc47afa9849a9d8771d084bb1fc28c03e0
Michael Chuang <michael16199@gmail.com>
2019-04-27 15:20:30 +0800

    create Navbar.vue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import 'bootstrap' // Import js file
+import 'bootstrap/dist/css/bootstrap.min.css' // Import css file
 Vue.config.productionTip = false
 
 new Vue({
